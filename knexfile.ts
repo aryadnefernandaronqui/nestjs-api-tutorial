687cc97b7c5ea737f6433adf5f3d1f93d5618f25
aryadnefernandaronqui <aryadne.fernanda@gmail.com>
2025-02-11 15:17:35 -0500

    added knex configurations

---
+++ knexfile.ts
@@ -0,0 +1,43 @@
+import dotenv from 'dotenv';
+import { Knex } from 'knex';
+
+dotenv.config();
+
+const config: { [key: string]: Knex.Config } = {
+  development: {
+    client: 'pg',
+    connection: {
+      host: process.env.DB_WRITE_HOST, 
+      port: Number(process.env.DB_PORT),
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: './migrations'
+    },
+    seeds: {
+      directory: './seeds'
+    }
+  },
+  readReplica: {
+    client: 'pg',
+    connection: {
+      host: process.env.DB_READ_HOST, 
+      port: Number(process.env.DB_PORT),
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME
+    },
+    pool: {
+      min: 2,
+      max: 10
+    }
+  }
+};
+
+export default config;
